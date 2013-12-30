a7a0b75297cbb66829145ef4a8b1aa0f7d3b3da4
Ethan Piliavin <ethan@piliavin.com>
2013-12-29 22:53:05 -0800

    v1.1 - Add support for NoFollow and HTTPS

--- links-with-icons-widget.php
+++ links-with-icons-widget.php
@@ -2,14 +2,14 @@
 /*
 Plugin Name: Links With Icons Widget
 Plugin URI: http://angeleswebdesign.com
-Description: A widget that lists links with icons. Based on Custom Link Widget by Pankaj Biswas.
+Description: A widget that lists links with icons.
 Author: Ethan Piliavin
 Author URI: http://piliavin.com
-Version: 1.0
+Version: 1.1
 */
 class wLWI extends WP_Widget {
 
-	const VERSION = '1.0.0';
+	const VERSION = '1.1.0';
 
 	function __construct(){
 		$options = array(
@@ -57,45 +57,54 @@ class wLWI extends WP_Widget {
 			<input type="number" min="1" max="20" class="widefat" style="background:#fff;width:40px;text-align:center;" id="<?php echo $this->get_field_id('numB');?>" name="<?php echo $this->get_field_name('numB');?>" value="<?php echo !empty($numB) ? $numB:1;?>"/>
 			<br><i>Save to See Additional Fields. NOTE: Reducing the number of links will delete the extra link data.</i>
 		</p>
-		<?php for($i=0;$i<$numB;$i++)
-		{
-			$count=$i+1;
-			$target = 'iT'.$count;
-			$link = 'iLink'.$count;
-			$name = 'iName'.$count;
-			$icon = 'iIcon'.$count;
-		?>
-		<hr />
-		<h4>Link #<?php echo $count;?> Details</h4>
+		<?php 
+			for($i=0;$i<$numB;$i++)
+			{
+				$count=$i+1;
+				$target = 'iT'.$count;
+				$link = 'iLink'.$count;
+				$name = 'iName'.$count;
+				$icon = 'iIcon'.$count;
+				$nofollow = 'iFollow'.$count;
+			?>
+			<hr />
+			<h4>Link #<?php echo $count;?> Details</h4>
 
-		<p>
-			<label for="<?php echo $this->get_field_id($link);?>">URL: (Include http://) </label>
-			<input class="widefat validate validate_url" style="background:#fff;" id="<?php echo $this->get_field_id($link);?>" name="<?php echo $this->get_field_name($link);?>" value="<?php if(isset($$link)) echo esc_attr($$link);?>"/>
-		</p>
-		
-				<!-- New Window Opening Option -->
-		<p>
+			<p>
+				<label for="<?php echo $this->get_field_id($link);?>">URL: (Include http://) </label>
+				<input class="widefat validate validate_url" style="background:#fff;" id="<?php echo $this->get_field_id($link);?>" name="<?php echo $this->get_field_name($link);?>" value="<?php if(isset($$link)) echo esc_attr($$link);?>"/>
+			</p>
 			
-			<input type="checkbox" class="checkbox" <?php checked($instance[$target], true) ?> id="<?php echo $this->get_field_id($target);?>" name="<?php echo $this->get_field_name($target);?>" value="1"/>
-			<label for="<?php echo $this->get_field_id($target);?>">Open In New Window</label>
-		</p>
-		<!-- /New Window Opening Option -->
+			<!-- New Window Opening Option: -->
+			<p>
+				
+				<input type="checkbox" class="checkbox" <?php checked($instance[$target], true) ?> id="<?php echo $this->get_field_id($target);?>" name="<?php echo $this->get_field_name($target);?>" value="1"/>
+				<label for="<?php echo $this->get_field_id($target);?>">Open In New Window</label>
+			</p>
+			<!-- /New Window Opening Option -->
 
-		
-		<p>
-			<label for="<?php echo $this->get_field_id($name);?>">Link Text: </label>
-			<input class="widefat" style="background:#fff;" id="<?php echo $this->get_field_id($name);?>" name="<?php echo $this->get_field_name($name);?>" value="<?php if(isset($$name)) echo esc_attr($$name);?>"/>
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id($icon);?>">Icon: </label>
-			<img id="<?php echo $this->get_field_id($icon);?>-preview" style="max-width:100%;" <?php if (isset($icon)) echo 'src="'.$$icon.'"'?> >
-			<br />
-			<input type="submit" class="button upload_image_button" value="<?php _e('Select Image', 'links_with_icons_widget'); ?>" data-target-id="<?php echo $this->get_field_id($icon);?>"/>
-			<input type="hidden" id="<?php echo $this->get_field_id($icon);?>" name="<?php echo $this->get_field_name($icon);?>" value="<?php echo $$icon; ?>" />
-		</p>
-		<?php
-		}?>
-		<?php
+			<!-- No Follow Option: -->
+			<p>
+				
+				<input type="checkbox" class="checkbox" <?php checked($instance[$nofollow], true) ?> id="<?php echo $this->get_field_id($nofollow);?>" name="<?php echo $this->get_field_name($nofollow);?>" value="1"/>
+				<label for="<?php echo $this->get_field_id($nofollow);?>">No Follow</label>
+			</p>
+			<!-- /New Window Opening Option: -->
+			
+			<p>
+				<label for="<?php echo $this->get_field_id($name);?>">Link Text: </label>
+				<input class="widefat" style="background:#fff;" id="<?php echo $this->get_field_id($name);?>" name="<?php echo $this->get_field_name($name);?>" value="<?php if(isset($$name)) echo esc_attr($$name);?>"/>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id($icon);?>">Icon: </label>
+				<img id="<?php echo $this->get_field_id($icon);?>-preview" style="max-width:100%;" <?php if (isset($icon)) echo 'src="'.$$icon.'"'?> >
+				<br />
+				<input type="submit" class="button upload_image_button" value="<?php _e('Select Image', 'links_with_icons_widget'); ?>" data-target-id="<?php echo $this->get_field_id($icon);?>"/>
+				<input type="hidden" id="<?php echo $this->get_field_id($icon);?>" name="<?php echo $this->get_field_name($icon);?>" value="<?php echo $$icon; ?>" />
+			</p>
+			<?php
+			}?>
+			<?php
 	}
 	
 	//Displaying The Data To Widget
@@ -115,6 +124,8 @@ class wLWI extends WP_Widget {
 			$link = 'iLink'.$count;
 			$name = 'iName'.$count;
 			$icon = 'iIcon'.$count;
+			$nofollow = 'iFollow'.$count;
+			
 			if(empty($$name)) return false;
 
 			//Determining Whether To Open In New Window Or Not
@@ -126,7 +137,22 @@ class wLWI extends WP_Widget {
 			{
 				$tar = '';
 			}
-			echo '<li class="link_with_icon"><img src="'.$$icon.'"><a '.$tar.'href="'.esc_attr($$link).'">'.esc_attr($$name).'</a></li>';
+			
+			//Determining If Link IS No Follow
+			if($$nofollow == 1) 
+			{
+				$fol = 'rel="nofollow" ';
+			}
+			else
+			{
+				$fol = '';
+			}			
+			
+			//Parse URL for proper output in HTTP or HTTPS environment
+			$imgurl = parse_url($$icon);
+			$imgsrc=$imgurl["host"].$imgurl["path"];
+						
+			echo '<li class="link_with_icon"><img src="//'.$imgsrc.'"><a '.$fol.$tar.'href="'.esc_attr($$link).'">'.esc_attr($$name).'</a></li>';
 		}
 		echo '</ul>';
 		echo $after_widget;
--- readme.txt
+++ readme.txt
@@ -1,19 +1,21 @@
 === Links With Icons Widget ===
-Contributors: ethanpil, pankajbiswas
-Tags: widget,widgets,link widget, icons, links with icons
+Contributors: ethanpil
+Tags: widget, widgets, link widget, icons, link with icon, icon link, links with icons, link, icon, link icon, link image, image, uploader, widget image, icon and link, nofollow
 Requires at least: 3.0
-Tested up to: 3.6.1
+Tested up to: 3.8
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Widgets to display links with icons.
+A widget to display links with icons alongside.
 
 == Description ==
 
 Links With Icons Widget adds a new custom widget which displays links with icons alongside. The links are dynamically added to each widget in the widget admin. This plugin does not use the links manager and does not create any custom post type to store the links. All data is self contained in each widget as widget option data. Images are added and managed using the media library and uploader.
 
+
 Usage:
+
 1. In the WordPress admin, go to Appearance...Widgets
 2. Drag the "Links With Icons" widget to a sidebar
 3. Select the number of links the widget will display and save the widget.
@@ -22,19 +24,24 @@ Usage:
 6. Viola! The widget now appears with your pretty icons.
 
 Important Notes:
+
 1. The widget doesn't care about icon / image size. You should probably keep it consistent (and small) so it will be pretty.
 2. If you reduce the number of links in a widget after it has been populated with data, the excess links data will be deleted forever.
 3. The default CSS class for the container UL is links-with-icons-widget
 4. Each link li has a class of link-with-icon
 5. You can style and override the defaults in your own custom css, like:
-	.links-with-icons { /* Style */ }
-    .link-with-icon a { /* Style */ }
-	.link-with-icon img { /* Style */ }
-     etc...
+
+`
+.links-with-icons { /* custom styles */ }
+.link-with-icon a { /* custom styles */ }
+.link-with-icon img { /* custom styles */ }
+etc...
+`
 	 
 Fork away: https://github.com/ethanpil/wp-links-with-icons-widget
 
 Future Plans:
+
 1. Display the size of selected image in the widget under the preview
 2. Choose icon position for each link (left (default) or right side)
 
@@ -47,7 +54,7 @@ Like every other Wordpress plugin! :)
 
 == Frequently Asked Questions ==
 
-1.  The link doesnt work! Its showing a 404 error!
+1.  The link doesn't work! Its showing a 404 error!
     Don't forget to add http:// to your link.
 
 2.  What if I decrease the number of links in a widget?
@@ -55,9 +62,15 @@ Like every other Wordpress plugin! :)
 	
 == Screenshots ==
 
-1. The widget in action.
+1. Managing links inside the widget
+2. Example Widget Output
 
 == Changelog ==
+
+= 1.1 =
+
+* Added option for No Follow links
+* Image URLs are now returned without protocol to support HTTP or HTTPS
 
 = 1.0 =
 
